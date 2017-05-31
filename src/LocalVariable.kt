900b36194ca37625d974da1c9413bcc8b7f695d1
Carlos Anguiano <carlos.anguiano@gmail.com>
2017-05-31 08:20:42 -0500

    Local Variable

---
+++ src/LocalVariable.kt
@@ -0,0 +1,12 @@
+/**
+ * Created by carlosjoseanguiano on 31/05/17.
+ */
+
+fun main(args: Array<String>) {
+    val a: Int = 1
+    val b = 2
+    val c: Int
+    c = 3
+
+    println("a = $a, b = $b, c=$c")
+}
